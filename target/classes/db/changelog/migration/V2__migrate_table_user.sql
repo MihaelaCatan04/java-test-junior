4ec5b4daa4eda284bcee83474ab995c99b2573b1
Mihaela Catan <66206241+MihaelaCatan04@users.noreply.github.com>
2026-02-11 11:02:28 +0200

    Delete target directory

--- target/classes/db/changelog/migration/V2__migrate_table_user.sql
+++
@@ -1,9 +0,0 @@
---
--- Name: user; Type: TABLE; Schema: public; Owner: postgres
---
-CREATE TABLE IF NOT EXISTS "users"
-(
-    id          BIGSERIAL PRIMARY KEY,
-    username    VARCHAR(255)   NOT NULL UNIQUE,
-    password    VARCHAR(255)   NOT NULL
-    );
