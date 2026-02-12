abe59ef96667668f89c9d0d42032d6c12c4845ad
Mihaela Catan <mihaelacatan7@gmail.com>
2026-02-12 10:09:32 +0200

    fix: Remove authentication for the GET /products/** endpoint

--- src/main/java/com/java/test/junior/configuration/SecurityConfig.java
+++ src/main/java/com/java/test/junior/configuration/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/error").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
                         .requestMatchers("/products/**").authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
